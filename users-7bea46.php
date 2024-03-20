dd097607b81f1bd0cc89b1f3c02c8f61672b8f71
leshalls <154438051+leshalls@users.noreply.github.com>
2024-03-20 13:12:23 +0100

    Delete users-7bea46.php

--- users-7bea46.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $users = array('matthieu' => array('id' => 1, 'name' => '', 'surname' => '', 'pwd' => 'eJQhPz2/2PMxfikoVZcelbRiCsWiwAhG', 'email' => '', 'info' => '', 'groups' => array('1')));
-    $salt = '$2y$10$o7j4vgztxfe7pZ8qaUaHyl$';
